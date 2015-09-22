48899b142003898e7d7de1e20037c8880d8df60f
jcastaldoFoodEssentials <jcastaldo@foodessentials.com>
2015-09-22 16:56:14 -0500

    Update zkconfigset.rb
    Forgot the parentheses

--- providers/zkconfigset.rb
+++ providers/zkconfigset.rb
@@ -53,10 +53,10 @@ action :create do
       only_if { node['solrcloud']['manage_zkconfigsets_source'] }
     end
 
-    Chef::Log.info("The configset name is #{new_resource.configset_name}"
-    Chef::Log.info("The solr zkcli is #{new_resource.solr_zkcli}"
-    Chef::Log.info("The zkconfigset_home is #{new_resource.zkconfigsets_home}"
-    Chef::Log.info("The zkhost is #{new_resource.zkhost}"
+    Chef::Log.info("The configset name is #{new_resource.configset_name}")
+    Chef::Log.info("The solr zkcli is #{new_resource.solr_zkcli}")
+    Chef::Log.info("The zkconfigset_home is #{new_resource.zkconfigsets_home}")
+    Chef::Log.info("The zkhost is #{new_resource.zkhost}")
 
     # Upload on any config update
     execute "zk_config_set_upconfig_#{new_resource.configset_name}_update_upload" do
